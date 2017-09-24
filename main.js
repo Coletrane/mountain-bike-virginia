b3174a7686e006c03145adea281d7ac991598a9a
Cole Inman <coletranemusic@gmail.com>
2017-09-24 07:42:44 -0500

    put environment variable back in main.js

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ const PORT = 9002;
 app.use(compression());
 app.use(express.static('dist'));
 
-app.listen(PORT, function() {
+app.listen(process.env.PORT || PORT, function() {
   console.log('Mountain Bike Virginia running on port', PORT);
 });
