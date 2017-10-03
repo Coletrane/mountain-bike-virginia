213e1c2a28b1c2b4850a735ea3fc9dad74169369
Cole Inman <cole.inman@digitalreasoning.com>
2017-10-03 14:17:57 -0400

    think I'm abbandoning this

--- dist/vendor.bundle.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([1],{251:function(n,o,p){n.exports=p(7)}},[251]);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './resources/styles.css';
+import '../node_modules/bulma/css/bulma.css';
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
