7bda34457720dfee743e5da3446921e2e269bfc2
Cole Inman <cole.inman@digitalreasoning.com>
2017-10-31 21:16:26 -0500

    building for deployment

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const appRoutes = [
   '/relaunch',
   '/creature2017recap',
   '/pivot-switchblade-review',
-  '/spec-mines-oct-29-2017'
+  // '/spec-mines-oct-29-2017'
 ]
 
 // Get image routes in sync with site routes
@@ -18,7 +18,7 @@ imgRoutes['results'] = './podium.png'
 imgRoutes['/relaunch'] = './rockymthi8.jpg'
 imgRoutes['/creature2017recap'] = './lumberjack.jpg'
 imgRoutes['/pivot-switchblade-review'] = './IMG_3009.jpg'
-imgRoutes['/spec-mines-oct-29-2017'] = './IMG_2795.jpg'
+// imgRoutes['/spec-mines-oct-29-2017'] = './IMG_2795.jpg'
 
 
 module.exports = {
