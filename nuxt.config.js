133fec2597efefee3892f5fc591456196f46bee2
Cole <eloc49@gmail.com>
2018-01-27 18:10:28 -0600

    removing alter-meta since facebook doesn't seem to work anyway

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,10 +39,6 @@ module.exports = {
     ],
     script: [
       {
-        src: "/alter-meta.js",
-        type: "text/javascript"
-      },
-      {
         src: "https://code.jquery.com/jquery-3.2.1.slim.min.js",
         integrity: "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN",
         crossorigin: "anonymous",
--- static/alter-meta.js
+++
@@ -1,1 +0,0 @@
-"use strict";(function(){var a=document.getElementsByTagName("meta");if(a)for(var b=0;b<a.length;b++)a[b].removeAttribute("data-n-head")})();
