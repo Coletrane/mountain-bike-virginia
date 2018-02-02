72e5cc330680517a89b49e15beddc45512823bce
Cole Inman <cole.inman@digitalreasoning.com>
2018-02-02 15:18:17 -0600

    loading jquery and bootstrap from vendor.js now

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,16 @@ const sitemapRoutes = routes.appRoutes.map(route => {
 })
 
 module.exports = {
+  css: [
+    {
+      src: 'bootstrap/dist/css/bootstrap.min.css',
+      lang: 'css'
+    },
+    {
+      src: 'font-awesome/css/font-awesome.css',
+      lang: 'css'
+    }
+  ],
   head: {
     meta: [
       {charset: 'utf-8'},
@@ -23,40 +33,24 @@ module.exports = {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css?family=Julius+Sans+One|Lato'
       },
-      {
-        rel: 'stylesheet',
-        href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-        integrity: 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN',
-        crossorigin: 'anonymous'
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-        integrity: 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm',
-        crossorigin: 'anonymous'
-      },
-      {
-        rel: 'stylesheet',
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css',
-      },
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ],
     script: [
-      {
-        src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-        integrity: 'sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN',
-        crossorigin: 'anonymous'
-      },
-      {
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-        integrity: 'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q',
-        crossorigin: 'anonymous'
-      },
-      {
-        src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
-        integrity: 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl',
-        crossorigin: 'anonymous'
-      }
+      // {
+      //   src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+      //   integrity: 'sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN',
+      //   crossorigin: 'anonymous'
+      // },
+      // {
+      //   src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+      //   integrity: 'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q',
+      //   crossorigin: 'anonymous'
+      // },
+      // {
+      //   src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+      //   integrity: 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl',
+      //   crossorigin: 'anonymous'
+      // }
     ]
   },
   plugins: [
@@ -65,7 +59,8 @@ module.exports = {
   build: {
     extractCss: true,
     vendor: [
-      'vue-lazyload'
+      'bootstrap/dist/js/bootstrap.min.js',
+      'jquery/dist/jquery.slim.js'
     ]
   },
   modules: [
@@ -74,7 +69,7 @@ module.exports = {
       {
         id: 'UA-107968765-1'
       }
-    ],
+    ]
   ],
   sitemap: {
     path: '/sitemap.xml',
