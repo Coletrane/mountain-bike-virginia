514c528c125139df720ee18833073f93378b631f
Cole <eloc49@gmail.com>
2018-05-16 07:38:37 -0400

    got scaffolding up

---
+++ components/PromoCards/Events/VaBlueRidgeMeetingMay172018.vue
@@ -0,0 +1,29 @@
+<template>
+  <post-card :post="post">
+    <div slot="words">
+      <div class="promo">
+
+      </div>
+    </div>
+    <div slot="media">
+      <image-link :post="post"/>
+    </div>
+  </post-card>
+</template>
+<script>
+  import PostCard from '../../Card/PostCard'
+  import ImageLink from '../../Images/ImageLink'
+
+  import promoCard from '../../../assets/mixins/promo-card'
+
+  export default {
+    name: 'events-va-blue-ridge-meeting-may-17-2018-promo',
+    components: {
+      PostCard,
+      ImageLink
+    },
+    mixins: [
+      promoCard
+    ]
+  }
+</script>
---
+++ pages/events/va-blue-ridge-meeting-may-17-2018.vue
@@ -0,0 +1,25 @@
+<template>
+  <blog-post :image="img + ''"
+             :post="post"
+             :related-posts="relatedPosts">
+    <div slot="content">
+    </div>
+  </blog-post>
+</template>
+<script>
+  import BlogPost from '../../components/BlogPost'
+  import BlogImage from '../../components/Images/BlogImage'
+
+  import blogPost from '../../assets/mixins/blog-post'
+
+  export default {
+    name: 'va-blue-ridge-meeting-may-17-2018',
+    components: {
+      BlogImage,
+      BlogPost
+    },
+    mixins: [
+      blogPost
+    ]
+  }
+</script>
