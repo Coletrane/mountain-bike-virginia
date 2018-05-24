7f60df0fe47263f6e64d4531fe45c1d074bc5657
Cole <eloc49@gmail.com>
2018-05-24 06:33:54 -0400

    added foggy spec post

---
+++ components/PromoCards/Videos/BlueRidgeTrailheadsFoggySpec.vue
@@ -0,0 +1,24 @@
+<template>
+  <post-card :post="post">
+    <div slot="media">
+      <youtube :src="post.ytSrc"/>
+    </div>
+  </post-card>
+</template>
+<script>
+  import PostCard from '../../Card/PostCard'
+  import Youtube from '../../Iframes/Youtube'
+
+  import promoCard from '../../../assets/mixins/promo-card'
+
+  export default {
+    name: 'videos-blue-ridge-trailheads-foggy-spec-promo',
+    components: {
+      PostCard,
+      Youtube
+    },
+    mixins: [
+      promoCard
+    ]
+  }
+</script>
