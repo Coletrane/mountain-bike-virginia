1ec8daebc7338a86df84a9ad78fc78e5b31e7303
Cole <eloc49@gmail.com>
2018-06-23 11:11:51 -0400

    FINALLY GOT IT WORKING

--- components/AbstractPage.vue
+++ components/AbstractPage.vue
@@ -2,22 +2,14 @@
   <div class="abstract-page">
     <div v-if="pageLoaded">
       <div v-for="(postRoute, i) of $store.state.posts.loadedPages[page]">
-        <transition-group name="new-page-fade">
-            <component v-if="customPromoCard(postRoute)"
-                       :is="getPostComponent(postRoute)"
-                       key="0"/>
-            <component v-else
-                       :is="getPostComponent(postRoute)"
-                       v-bind="{
+        <transition name="new-page-fade">
+          <component v-if="getPostComponent(postRoute)"
+                     :is="getPostComponent(postRoute)"
+                     v-bind="{
                          postRoute: postRoute
-                       }"
-                       key="1"/>
-        </transition-group>
+                       }"/>
+        </transition>
         <banner v-if="(i + 1) % 3 === 0"/>
-        <div v-if="!getPostComponent(postRoute) &&
-                    getPostComponent(postRoute) !== 404"
-             class="post-card-placeholder">
-        </div>
       </div>
     </div>
   </div>
@@ -37,55 +29,57 @@
       }
     },
     components: {
-      Banner,
+      Banner
     },
     data() {
       return {
         postComponents: []
       }
     },
-    async created() {
-      for (const postRoute of this.$store.state.posts.loadedPages[this.page]) {
-        await this.loadPostComponent(postRoute)
-      }
-    },
     computed: {
-      pageLoaded() {
+      async pageLoaded() {
         const loadedPage = this.$store.state.posts.loadedPages[this.page]
         const loadedPosts = this.$store.getters.posts(loadedPage)
-        return loadedPage &&
-               loadedPosts &&
-               loadedPosts.length === loadedPage.length
+        const pageLoaded = loadedPage &&
+                           loadedPosts &&
+                           loadedPosts.length === loadedPage.length
+        if (pageLoaded) {
+          for (const postRoute of loadedPage) {
+            await this.loadPostComponent(postRoute)
+          }
+        }
+
+        return pageLoaded
       }
     },
     methods: {
       async loadPostComponent(postRoute) {
-        const filename = routeToComponentFilename(postRoute)
-        let postComponent = {
-          route: postRoute
-        }
-        try {
-          const esModule = await import(`@/components/PromoCards/${filename}`)
-          postComponent.component = esModule.default
-        } catch (err) {
-          // No custom PostCard component
-          const post = this.$store.getters.posts(postRoute)
-          if (!post) {
-            console.log(postRoute, this.$store.getters.posts(postRoute))
-            console.log(this.$store.state.posts.loadedPosts.find(post => post.route === postRoute) )
+        let post = this.$store.getters.posts(postRoute)
+        
+        if (post) {
+          const filename = routeToComponentFilename(postRoute)
+          let postComponent = {
+            route: postRoute
           }
-          if (post && post.ytSrc) {
-            const videoPromo = await import('@/components/Card/VideoPromoCard')
-            postComponent.component = videoPromo.default
-          } else if (post && post.schema.type === schemaTypes.article) {
-            const blogPromo = await import('@/components/Card/BlogPostPromoCard')
-            postComponent.component = blogPromo.default
-          } else {
-            // Website is in middle of a deployment and routes json is out of sync with .js files on the server
-            postComponent.component === 404
+          try {
+            const esModule = await import(`@/components/PromoCards/${filename}`)
+            postComponent.component = esModule.default
+          } catch (err) {
+            // No custom PostCard component
+            if (post.ytSrc) {
+              const videoPromo = await import('@/components/Card/VideoPromoCard')
+              postComponent.component = videoPromo.default
+            } else if (post.schema.type === schemaTypes.article) {
+              const blogPromo = await import('@/components/Card/BlogPostPromoCard')
+              postComponent.component = blogPromo.default
+            } else {
+              // Website is in middle of a deployment and routes json is out of sync with .js files on the server
+              const cardPlaceholder = await import('@/components/Card/PostCardPlaceholder')
+              postComponent.component = cardPlaceholder.default
+            }
+          } finally {
+            this.postComponents.push(postComponent)
           }
-        } finally {
-          this.postComponents.push(postComponent)
         }
       },
       getPostComponent(postRoute) {
@@ -96,25 +90,11 @@
         if (postComponent && postComponent.component) {
           return postComponent.component
         }
-      },
-      customPromoCard(postRoute) {
-        const postComponent = this.getPostComponent(postRoute)
-        // TODO: evaluate optional chaining here
-        return postComponent &&
-               postComponent.component &&
-               postComponent.component !== 404 &&
-               postComponent.component.name !== 'video-promo-card' &&
-               postComponent.component.name !== 'blog-post-promo-card'
       }
     }
   }
 </script>
 <style>
-  .post-card-placeholder {
-    width: 100%;
-    height: 500px;
-  }
-
   .new-page-fade-enter-to {
     transition: opacity 1s;
   }
---
+++ components/Card/PostCardPlaceholder.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="post-card-placeholder"></div>
+</template>
+<script>
+  export default {
+    name: 'post-card-placeholder'
+  }
+</script>
+<style>
+  .post-card-placeholder {
+    width: 100%;
+    height: 500px;
+  }
+</style>
