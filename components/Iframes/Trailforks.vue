f4362c5dcb0b89a145574785700dd0203e0c048b
Cole <eloc49@gmail.com>
2018-08-21 11:35:43 -0400

    committing before I go full iframe on your ass

--- components/Iframes/Trailforks.vue
+++ components/Iframes/Trailforks.vue
@@ -1,81 +1,74 @@
 <template>
   <div class="trailforks"
        :style="style">
-    <div class="TrailforksRegionInfo trailforks-widget"
-         data-rid="3159"
-         data-counts="1"
-         data-stats="1"
-         data-w="100%"
-         data-h="150px">
-    </div>
-    <div v-if="mobile" class="two-fingers">
-      Use 2 fingers to move the map
-    </div>
-    <div class="TrailforksWidgetMap trailforks-widget"
-         data-rid="3159"
-         data-maptype="osm"
-         data-trailstyle="difficulty"
-         data-controls="1"
-         data-list="0"
-         data-dml="1"
-         data-layers="trail,labels,photo,poi,directory,region"
-         data-w="100%"
-         data-z="8"
-         data-lat=""
-         data-lon=""
-         data-hideunsanctioned="">
-    </div>
+    <!--<div class="TrailforksRegionInfo trailforks-widget"-->
+         <!--data-rid="3159"-->
+         <!--data-counts="1"-->
+         <!--data-stats="1"-->
+         <!--data-w="100%"-->
+         <!--data-h="150px">-->
+    <!--</div>-->
+    <!--<div class="TrailforksWidgetMap trailforks-widget"-->
+         <!--data-rid="3159"-->
+         <!--data-maptype="osm"-->
+         <!--data-trailstyle="difficulty"-->
+         <!--data-controls="1"-->
+         <!--data-list="0"-->
+         <!--data-dml="1"-->
+         <!--data-layers="trail,labels,photo,poi,directory,region"-->
+         <!--data-w="100%"-->
+         <!--data-z="8"-->
+         <!--data-lat=""-->
+         <!--data-lon=""-->
+         <!--data-hideunsanctioned="">-->
+    <!--</div>-->
+    <iframe src="https://www.trailforks.com/widgets/region_info/?rid=3159&amp;counts=1&amp;stats=1&amp;w=100%&amp;h=150px&amp;title=null" scrolling="no" frameborder="0" allowfullscreen="1" width="100%" height="150px" id="regioninfo0"></iframe>
+    <div data-rid="3159" data-maptype="osm" data-trailstyle="difficulty" data-controls="1" data-list="0" data-dml="1" data-layers="trail,labels,photo,poi,directory,region" data-w="100%" data-z="8" data-lat="" data-lon="" data-hideunsanctioned="" class="TrailforksWidgetMap trailforks-widget" data-v-5c21f6b4="" data-h="800" style="width: 100%;"><iframe src="https://www.trailforks.com/widgets/region_map/?rid=3159&amp;w=100%&amp;h=800&amp;maptype=osm&amp;trailstyle=difficulty&amp;controls=1&amp;list=0&amp;layers=trail,labels,photo,poi,directory,region&amp;hideunsanctioned=&amp;z=8&amp;lat=&amp;lon=" scrolling="no" frameborder="0" allowfullscreen="1" width="100%" height="800" id="map0"></iframe></div>
   </div>
 </template>
 <script>
-import isMobile from '../../assets/detect-mobile'
-
 export default {
-  name: 'trailforks',
+  name: "trailforks",
   data() {
     return {
-      scriptSrc: 'https://es.pinkbike.org/ttl-86400/sprt/j/trailforks/widget.js'
-
+      scriptSrc: "https://es.pinkbike.org/ttl-86400/sprt/j/trailforks/widget.js"
     }
   },
   mounted() {
     if (process.browser) {
       this.$el
-        .getElementsByClassName('TrailforksWidgetMap')[0]
-        .setAttribute('data-h', parseInt(this.style.height) - 300)
+        .getElementsByClassName("TrailforksWidgetMap")[0]
+        .setAttribute("data-h", parseInt(this.style.height) - 300)
 
-      if (this.$store.state.misc.loaded) {
-        this.attachScript()
-      } else {
-        this.$store.watch(state => state.misc.loaded, this.attachScript)
-      }
+      // if (this.$store.state.misc.loaded) {
+      //   this.attachScript()
+      // } else {
+      //   this.$store.watch(state => state.misc.loaded, this.attachScript)
+      // }
     }
   },
   destroyed() {
-    this.detachScript()
+    if (process.browser) {
+      window.widgetScriptLoaded = false
+    }
   },
   methods: {
     attachScript() {
       if (!this.scriptLoaded) {
-        const script = document.createElement('script')
-        script.setAttribute('src', this.scriptSrc)
-        document.getElementsByTagName('head')[0].appendChild(script)
-        const widgetCheck = false
+        const script = document.createElement("script")
+        script.setAttribute("src", this.scriptSrc)
+        document.getElementsByTagName("head")[0].appendChild(script)
       }
-    },
-    detachScript() {
-      if (this.scriptLoaded) {
-        document.removeChild(this.scriptLoaded)
-      }
+
     }
   },
   computed: {
     scriptLoaded() {
       if (process.browser) {
         return Array.prototype.slice
-          .call(document.getElementsByTagName('script'))
+          .call(document.getElementsByTagName("script"))
           .find(scriptEl => {
-            return scriptEl.getAttribute('src') === this.scriptSrc
+            return scriptEl.getAttribute("src") === this.scriptSrc
           })
       }
     },
@@ -83,29 +76,29 @@ export default {
       if (process.browser) {
         let style = {}
         if (document.body.clientWidth > 1020) {
-          style.width = '90%'
-          style.height = '1300px'
+          style.width = "90%"
+          style.height = "1300px"
         } else if (
           document.body.clientWidth <= 1020 &&
           document.body.clientWidth > 720
         ) {
-          style.width = '90%'
-          style.height = '1100px'
+          style.width = "90%"
+          style.height = "1100px"
         } else if (
           document.body.clientWidth <= 720 &&
           document.body.clientWidth > 400
         ) {
-          style.width = '100%'
-          style.height = '1000px'
+          style.width = "100%"
+          style.height = "1000px"
         } else if (
           document.body.clientWidth <= 400 &&
           document.body.clientWidth > 350
         ) {
-          style.width = '100%'
-          style.height = '900px'
+          style.width = "100%"
+          style.height = "900px"
         } else if (document.body.clientWidth <= 350) {
-          style.width = '100%'
-          style.height = '750px'
+          style.width = "100%"
+          style.height = "750px"
         }
         return style
       }
